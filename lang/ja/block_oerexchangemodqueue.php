648120dc6ac9a15631ab998622989b47085a3b2c
Adam Jenkins <adam@wisecat.net>
2026-07-18 10:56:02 +0000

    Add Japanese (ja) language pack

---
+++ lang/ja/block_oerexchangemodqueue.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Japanese language strings for block_oerexchangemodqueue.
+ *
+ * @package    block_oerexchangemodqueue
+ * @copyright  2026 Adam Jenkins <adam@wisecat.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modqueue_deletedresource'] = '（削除されたリソース）';
+$string['modqueue_failedparses'] = '解析失敗（{$a} 件）';
+$string['modqueue_openreports'] = '未対応の報告（{$a} 件）';
+$string['modqueue_pendingsites'] = '承認待ちのサイト登録（{$a} 件）';
+$string['oerexchangemodqueue:addinstance'] = '新しい OER Exchange モデレーションキューブロックを追加する';
+$string['oerexchangemodqueue:myaddinstance'] = '新しい OER Exchange モデレーションキューブロックをダッシュボードに追加する';
+$string['pluginname'] = 'OER Exchange: モデレーションキュー';
+$string['privacy:metadata'] = 'この OER Exchange モデレーションキューブロックは、local_oerexchange によってすでに保存されているデータを表示するのみであり、独自にデータを保存することはありません。';
